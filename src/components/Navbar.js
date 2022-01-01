4e3f297f3db1eb97d5272f075ce6a22b7c3607c2
Preeti Mallick <preetimallick@Preetis-MacBook-Pro.local>
2022-01-01 16:04:48 +0530

    added two buttons next and previous and functionality to go to next page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'
+// import PropTypes from 'prop-types'
 
 export class Navbar extends Component {
     static propTypes = {
@@ -9,7 +9,7 @@ export class Navbar extends Component {
     render() {
         return (
             <div>
-                <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
   <div className="container-fluid">
     <a className="navbar-brand" href="/">NewsMonkey</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
