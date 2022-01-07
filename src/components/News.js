c9dea3162652e3e34f83dbb09202a3ab6dd88232
Preeti Mallick <preetimallick@Preetis-MacBook-Pro.local>
2022-01-07 17:03:16 +0530

    refactor the code of componentDidMount()

--- src/components/News.js
+++ src/components/News.js
@@ -36,8 +36,7 @@ export class News extends Component {
         }
     }
 
-    async componentDidMount(){
-        console.log("cdm");
+    async updateNews (){
         let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=911212c02dcc48a187faeac1580b8de5&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         this.setState({loading:true});
         let data = await fetch(url);
@@ -50,33 +49,18 @@ export class News extends Component {
     })
     }
 
-    handlePrevClick= async ()=>{
-        console.log("previous");
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=911212c02dcc48a187faeac1580b8de5&page=${this.state.page - 1}&pageSize=${this.props.pageSize}`;
-        this.setState({loading:true});
-        let data = await fetch(url);
-        let parsedData = await data.json()
-        console.log(parsedData);
-        this.setState({
-            page:this.state.page - 1,
-            articles: parsedData.articles,
-            loading: false
-        })
+    async componentDidMount(){
+        this.updateNews();
     }
 
+    handlePrevClick= async ()=>{
+            this.setState({page: this.state.page - 1});
+            this.updateNews();
+        }
+
     handleNextClick= async ()=>{
-        console.log("next");
-        if(!(this.state.page +1 > Math.ceil(this.state.totalResults/this.props.pageSize))){
-            let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=911212c02dcc48a187faeac1580b8de5&page=${this.state.page + 1}&pageSize=${this.props.pageSize}`;
-            this.setState({loading:true});
-            let data = await fetch(url);
-            let parsedData = await data.json()
-            this.setState({
-                page: this.state.page + 1,
-                articles: parsedData.articles,
-                loading: false
-        })
-    }
+        this.setState({page: this.state.page + 1});
+        this.updateNews();
     }
             
 
@@ -91,7 +75,7 @@ export class News extends Component {
                 <div className="row">
                     {!this.state.loading && this.state.articles.map((element)=>{
                     return <div className="col-md-4" key={element.url}>
-                    <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url}/>
+                    <NewsItem title={element.title?element.title:""} description={element.description?element.description:""} imageUrl={element.urlToImage} newsUrl={element.url} author={element.author} date={element.publishedAt} source={element.source.name}/>
                     </div>
                     })}
                     
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react'
 
 export class NewsItem extends Component {
     render() {
-        let {title, description,imageUrl,newsUrl} = this.props;
+        let {title, description,imageUrl,newsUrl,author,date,source} = this.props;
         return (
             <div className="my-3">
                 <div className="card" >
                     <img src={imageUrl} className="card-img-top" alt="..."/>
                     <div className="card-body">
                     <h5 className="card-title">{title}</h5>
+                    <span className ="position-absolute top-0 translate-middle badge rounded-pill bg-danger" style={{left: '90%', zIndex: '1'}}>{source} </span>
+                    
                     <p className="card-text">{description}</p>
+                    <p className="card-text"><small className="text-muted">By {!author?"Unknown":author} on {new Date(date).toGMTString()}</small></p>
                     <a href={newsUrl} target= "_blank" rel="noreferrer" className="btn btn-sm btn-dark">Read More</a>
                     </div>
                 </div>
